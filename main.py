fb2bb6efff63e8bd7c89ad44592125dfb303a77c
Packialakshmi <spackialakshmi1999@gmail.com>
2025-11-19 17:02:50 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from config import settings
-from langchain_cohere import ChatCohere
-
-llm = ChatCohere(cohere_api_key = settings.cohere_api_key)
-
-response = llm.invoke("what is the capital of India")
-
-print(response.content)
